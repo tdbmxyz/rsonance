3629e15941e056f20cccb00f7f58cd99b3653879
Tibo <tibo@tdbm.xyz>
2025-08-02 17:06:33 +0200

    chore(devenv): add clang to the stdenv

--- devenv.nix
+++ devenv.nix
@@ -3,11 +3,18 @@
   packages = with pkgs; [
     git
     alsa-lib
+    pipewire
   ];
+
+  stdenv = pkgs.clang19Stdenv;
 
   # https://devenv.sh/languages/
   languages.rust = {
     enable = true;
     components = ["rustc" "cargo" "clippy" "rustfmt" "rust-analyzer"];
+  };
+
+  env = {
+    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   };
 }
